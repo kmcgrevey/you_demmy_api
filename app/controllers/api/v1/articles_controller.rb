e57130178762a4a2f914e449364127b68f74c747
Kevin McGrevey <niv3kmcg@gmail.com>
2021-03-16 00:30:10 -0600

    Adds index and show function and testing for Articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::ArticlesController < ApplicationController
-
+  
+  def index
+    render json: ArticleSerializer.new(Article.all)
+  end
+  
+  def show
+    article = Article.find(params[:id])
+    render json: ArticleSerializer.new(article)
+  end
 
 end
---
+++ spec/requests/api/v1/articles_spec.rb
@@ -0,0 +1,47 @@
+# json_body defined in ApiHelpers module to DRY, added to rails_helper
+
+require "rails_helper"
+
+RSpec.describe Api::V1::ArticlesController, type: :request do
+  describe "#index" do
+    it "returns a succesful response" do
+      get "/api/v1/articles"
+      
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns a list of articles" do
+      article1 = create(:article)
+      article2 = create(:article)
+
+      get "/api/v1/articles"
+
+      expect(json_body[:data].class).to be(Array)
+      expect(json_body[:data].count).to eq(2)
+      expect(json_body[:data].first[:type]).to eq("article")
+    end
+  end
+
+  describe "#show" do
+    let(:article) { create(:article) }
+    
+    it "returns a successful response" do
+      get "/api/v1/articles/#{article.id}"
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns a proper JSON response" do
+      get "/api/v1/articles/#{article.id}"
+
+      expect(json_body[:data][:id]).to eq(article.id.to_s)
+      expect(json_body[:data][:type]).to eq("article")
+      expect(json_body[:data][:attributes]).to eq(
+        title: article.title,
+        content: article.content,
+        slug: article.slug
+      )
+    end      
+  end
+
+end
