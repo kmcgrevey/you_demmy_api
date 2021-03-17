faddad0d268851f0217d1c701c3a8495dfef2ea9
Kevin McGrevey <niv3kmcg@gmail.com>
2021-03-16 22:20:19 -0600

    Creates spec for Article page pagination

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -23,6 +23,26 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
       ids = json_body[:data].map { |article| article[:id].to_i }
       expect(ids).to eq([article2.id, article1.id])
     end
+
+    it "paginates results" do
+      article1, article2, article3 = create_list(:article, 3)
+
+      get "/api/v1/articles", params: { page: { number: 2, size: 1 } }
+
+      expect(json_body[:data].length).to eq(1)
+      expect(json_body[:data].first[:id]).to eq([article2.id])
+    end
+
+    it "contains pagination links in the response" do
+      article1, article2, article3 = create_list(:article, 3)
+
+      get "/api/v1/articles", params: { page: { number: 2, size: 1 } }
+
+      expect(json_body[:links].length).to eq(5)
+      expect(json_body[:links].keys).to contain_exactly(
+        "self", "first", "prev", "next", "last"
+        )
+    end
   end
 
   describe "#show" do
