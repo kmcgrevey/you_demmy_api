424a9a00231b070048ddf1e7c18c1ea5e1dab5eb
Kevin McGrevey <niv3kmcg@gmail.com>
2021-03-19 11:48:56 -0600

    Adds access_tokens route, controller and empty test setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post "/login", to: "access_tokens#create"
       resources :articles, only: [:index, :show]
     end
   end
