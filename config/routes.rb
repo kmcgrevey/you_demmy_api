ddb802b021ceccefa6b1f3997e3745902af4f54c
Kevin McGrevey <niv3kmcg@gmail.com>
2021-03-20 09:28:52 -0600

    Adds logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "/login", to: "access_tokens#create"
+      delete "/logout", to: "access_tokens#destroy"
       resources :articles, only: [:index, :show]
     end
   end
--- spec/routing/access_tokens_spec.rb
+++ spec/routing/access_tokens_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe "access tokens routes" do
   it "should route to access_tokens#create" do
     expect(post "/api/v1/login").to route_to("api/v1/access_tokens#create")
   end
-  
+
+  it "should route to Access-tokens#destroy" do
+    expect(delete "/api/v1/logout").to route_to("api/v1/access_tokens#destroy")
+  end
+
 end
