425bfec119b5a849e19b78d2bf374a1649cd87a8
Kevin McGrevey <niv3kmcg@gmail.com>
2021-03-19 19:05:11 -0600

    Adds function to return 201 status for successful login

--- app/controllers/api/v1/access_tokens_controller.rb
+++ app/controllers/api/v1/access_tokens_controller.rb
@@ -3,5 +3,7 @@ class Api::V1::AccessTokensController < ApplicationController
   def create
     auth = UserAuthenticatorService.new(params[:code])
     auth.perform
+
+    render json: {}, status: 201
   end
 end
--- spec/controllers/access_tokens_spec.rb
+++ spec/controllers/access_tokens_spec.rb
@@ -26,7 +26,29 @@ RSpec.describe Api::V1::AccessTokensController, type: :controller do
     end
 
     context "when successful request" do
+      let(:user_data) do
+        {
+          login: 'jsmith1',
+          url: 'http://example.com',
+          avatar_url: 'http://example.com/avatar',
+          name: 'John Smith'
+        }
+      end
 
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return('validaccesstoken')
+
+        allow_any_instance_of(Octokit::Client).to receive(
+          :user).and_return(user_data)
+      end
+
+      subject { post :create, params: { code: 'valid_code' } }
+
+      it "should return 201 status code" do
+        subject
+        expect(response).to have_http_status(:created)
+      end
     end
   end
 
