858e5f282e201f344274f07106602df40fdca101
Kevin McGrevey <niv3kmcg@gmail.com>
2021-03-29 16:20:33 -0600

    Adds password encryption handling

--- app/services/user_authenticator_service/standard.rb
+++ app/services/user_authenticator_service/standard.rb
@@ -1,6 +1,8 @@
 class UserAuthenticatorService::Standard < UserAuthenticatorService
   class AuthenticationError < StandardError; end
 
+  attr_reader :user, :access_token
+
   def initialize(login, password)
 
   end
