9d7297dd830800cde4d65a1a937b26598b7e6b55
Kevin McGrevey <niv3kmcg@gmail.com>
2021-04-02 20:29:10 -0600

    Adds route and controller for standard registration

---
+++ app/controllers/api/v1/registration_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::RegistrationController < ApplicationController
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "/login", to: "access_tokens#create"
       delete "/logout", to: "access_tokens#destroy"
+      post "/sign_up", to: "registration#create"
+      
       resources :articles do
         resources :comments, only: [:create, :index]
       end
