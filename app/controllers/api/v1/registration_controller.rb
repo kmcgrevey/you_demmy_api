83378e86df9ecc2fa9e800f03fc84dd863e74e51
Kevin McGrevey <niv3kmcg@gmail.com>
2021-04-03 09:00:25 -0600

    Completes standard login

--- app/controllers/api/v1/registration_controller.rb
+++ app/controllers/api/v1/registration_controller.rb
@@ -1,4 +1,22 @@
 class Api::V1::RegistrationController < ApplicationController
+  skip_before_action :authorize!, only: :create
 
+  def create
+    user = User.new(registration_params.merge(provider: "standard"))
+    user.save!
+    render json: user, status: 201
+  rescue ActiveRecord::RecordInvalid
+    render json: user, adapter: :json_api,
+    serializer: ErrorSerializer,
+    status: 422
+  end
+
+  private
+
+  def registration_params
+    params.require(:data).require(:attributes).
+      permit(:login, :password) ||
+      ApplicationController::Parameters.new
+  end
 
 end
